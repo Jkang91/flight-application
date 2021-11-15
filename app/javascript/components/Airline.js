9d182c8ed6886196dbee5f5e4149c1f42d2eb4ef
jkang91 <jkang34@gmail.com>
2021-11-15 15:26:04 -0500

    styled header and airline components

--- app/javascript/components/Airline.js
+++ app/javascript/components/Airline.js
@@ -1,6 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "./Header";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+const Column = styled.div`
+  background: #fff;
+  height: 100vh;
+  overflow: scroll;
+
+  &:last-child {
+    background: #000;
+  }
+`;
+const Main = styled.div`
+  padding-left: 50px;
+`;
 
 function Airline(props) {
   const [airline, setAirline] = useState({});
@@ -19,15 +39,22 @@ function Airline(props) {
       .catch((resp) => console.log(resp));
   }, []);
   return (
-    <div className="wrapper">
-      <div className="column">
-        {loaded && <Header attributes={airline.data.attributes} />}
-        <div className="reviews"></div>
-      </div>
-      <div className="column">
+    <Wrapper>
+      <Column>
+        <Main>
+          {loaded && (
+            <Header
+              attributes={airline.data.attributes}
+              reviews={airline.included}
+            />
+          )}
+          <div className="reviews"></div>
+        </Main>
+      </Column>
+      <Column>
         <div className="review-form">Review goes here</div>
-      </div>
-    </div>
+      </Column>
+    </Wrapper>
   );
 }
 
